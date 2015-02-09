7745ea8770e1dae3f919950e7576f593de634ee4
QasimK <QasimK@users.noreply.github.com>
2015-02-09 14:41:44 +0000

    Fix hotkey registration in console program

--- src/run_with_console.py
+++ src/run_with_console.py
@@ -4,16 +4,22 @@ import settings
 import windowinfo
 import hotkey
 
+logging = logging.getLogger(__name__)
+
 
 def move(x, y):
     windowinfo.fill_foreground_window()
 
 if __name__ == '__main__':
     config = settings.Settings()
-    config.setup_logging()
+    config.setup_logging(to_file=True, to_console=True)
     user_hotkey = config.get_hotkey()
     
-    if hotkey.register_hotkey(user_hotkey, move):
+    try:
+        hotkey.register_hotkey(user_hotkey, move)
+    except hotkey.FailedToRegisterHotkey as e:
+        logging.critical(e)
+    else:
         while True:
             try:
                 hotkey.process_next_message()
@@ -22,7 +28,5 @@ if __name__ == '__main__':
 
         if not hotkey.unregister_hotkey(keys=user_hotkey):
             logging.error("Unable to unregister hotkey")
-    else:
-        logging.critical("Unable to register hotkey")
     
     logging.shutdown()
